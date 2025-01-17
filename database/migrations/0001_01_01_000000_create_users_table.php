6157355829329e98cb6c034920404e170162a7e6
gdsousa <gdsousa@recordbrasilia.com.br>
2025-01-17 14:00:02 -0300

    feat: oath terminado

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('google_id')->nullable();
             $table->string('google_token')->nullable();
             $table->string('google_refresh_token')->nullable();
-            $table->rememberToken();
+            $table->string('status');
             $table->timestamps();
         });
 
